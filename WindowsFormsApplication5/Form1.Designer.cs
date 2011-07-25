ceaebda3c708dbe823821385bdb6fbc25464a7de
walx <walx.wang@gmail.com>
2011-07-25 15:04:54 +0800

    V1

--- WindowsFormsApplication5/Form1.Designer.cs
+++ WindowsFormsApplication5/Form1.Designer.cs
@@ -34,7 +34,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(155, 54);
+            this.button1.Location = new System.Drawing.Point(156, 65);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
